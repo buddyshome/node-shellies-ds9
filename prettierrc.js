3c9a8755020ee626c11c5572d0220b1ef8f3487c
Alexander Link <alex@linkzuhause.com>
2025-11-28 21:55:34 +0100

    prettier

--- prettierrc.js
+++ prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   printWidth: 140,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   tabWidth: 2,
   semi: true,
   singleQuote: true,
   tabs: false,
   bracketSpacing: true,
   jsxBracketSameLine: true,
-  arrowParens: 'avoid',
-  endOfLine: 'crlf',
+  arrowParens: "avoid",
+  endOfLine: "crlf",
 };
